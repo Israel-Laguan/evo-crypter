aa331b07e90beb90709b616a9f98e5ef9a2f9811
EricFerreyra <yoelferreya24@gmail.com>
2025-04-17 13:21:46 -0300

    feat: adding new variable to test merge

--- src/main.c
+++ src/main.c
@@ -4,6 +4,7 @@
 #include "mutations/mod.h"
 
 int test = 0; // Global variable for testing purposes
+int test2 = 0; // Global variable for testing purposes
 
 int main(int argc, char* argv[]) {
   Config config = initialize_config(argc, argv);
