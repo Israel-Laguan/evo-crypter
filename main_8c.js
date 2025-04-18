1695573d65a1d2fff4f30da707fcf479d3a0626b
YoelFerreyra <YoelFerreyra@users.noreply.github.com>
2025-04-18 19:18:50 +0000

    deploy: 48c55907eb6a54aff1af23994c3cd4d60cbd794b

--- main_8c.js
+++ main_8c.js
@@ -1,4 +1,5 @@
 var main_8c =
 [
-    [ "main", "main_8c.html#a0ddf1224851353fc92bfbff6f499fa97", null ]
+    [ "main", "main_8c.html#a0ddf1224851353fc92bfbff6f499fa97", null ],
+    [ "test", "main_8c.html#a1bd3f5fbccb1628eb13dda4cd02633a4", null ]
 ];
--- search/variables_9.js
+++ search/variables_9.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['threads_0',['threads',['../structConfig.html#acca8518fb474236ff23870135c29d73d',1,'Config']]]
+  ['test_0',['test',['../main_8c.html#a1bd3f5fbccb1628eb13dda4cd02633a4',1,'main.c']]],
+  ['threads_1',['threads',['../structConfig.html#acca8518fb474236ff23870135c29d73d',1,'Config']]]
 ];
